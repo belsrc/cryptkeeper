65ea4bd418806168c93dd82d47ad7f3326fc8f17
Bryan Kizer <bryan@bryanckizer.com>
2016-09-30 12:51:00 -0400

    change to promises added argon2

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## Cryptkeeper
 Some simple wrapper methods to help manage some crypto and crypto-random functions.
 
+##### Compatibility
+Version >= 1.0.0 now returns Promises and is incompatible with anything using version < 1.0.0
+
 ### Install
 -----------------------------------------------------
 
@@ -14,60 +17,97 @@ Then simply include the module and add it into the application.
 var cryptkeeper = require('cryptkeeper');
 ```
 
+### Testing
+-----------------------------------------------------
+
+To test simply run ```npm test```.
+
 ### Methods
 -----------------------------------------------------
 
 #### #base64Encode(val:Mixed)
 Base64 encodes the given value. ```val``` can be a Buffer or a string. If an object or number is provided then it will be stringified before encoding.
 ```javascript
-keeper.base64Encode('Hello world')
-// SGVsbG8gd29ybGQ=
+keeper
+  .base64Encode('Hello world')
+  .then(result => {
+    console.log(result);
+    // SGVsbG8gd29ybGQ=
+  });
 ```
 
 
 #### #base64Decode(val:String)
 Base64 decodes the given value.
 ```javascript
-keeper.base64Decode('SGVsbG8gd29ybGQ=')
-// Hello world
+keeper
+  .base64Decode('SGVsbG8gd29ybGQ=')
+  .then(result => {
+    console.log(result);
+    // Hello world
+  });
 ```
 
 
 #### #generateV4UUID()
 Generates a crypto-random V4 UUID.
 ```javascript
-keeper.generateV4UUID()
-// f2d54c65-ee01-48dd-8d8c-f46322ac6cf8
+keeper
+  .generateV4UUID()
+  .then(result => {
+    console.log(result);
+    // f2d54c65-ee01-48dd-8d8c-f46322ac6cf8
+  });
 ```
 
 
 #### #randomNumber([numOne:Number[, numTwo:Number]])
 Generates a crypto-random number. If no parameters are given then the returned number is between 0 and 1. If a parameter is given, the returned number is from 0 to the given number. If two parameters are given, the returned number is between the first number and the second number.
 ```javascript
-keeper.randomNumber()
-// 0.30741933521631026
+keeper
+  .randomNumber()
+  .then(result => {
+    console.log(result);
+    // 0.30741933521631026
+  });
 
-keeper.randomNumber(10)
-// 7
+keeper
+  .randomNumber(10)
+  .then(result => {
+    console.log(result);
+    // 7
+  });
 
-keeper.randomNumber(10, 20)
-// 19
+keeper
+  .randomNumber(10, 20)
+  .then(result => {
+    console.log(result);
+    // 19
+  });
 ```
 
 
 #### #randomHex(numBytes:Number)
 Generates a crypto-random hex string that is the given bytes long.
 ```javascript
-keeper.randomHex(16);
-// 77a534562141991263d1542546f8920b
+keeper
+  .randomHex(16)
+  .then(result => {
+    console.log(result);
+    // 77a534562141991263d1542546f8920b
+  });
 ```
 
 
 #### #randomBase64(numBytes:Number)
 Generates a crypto-random base 64 string that is the given bytes long.
 ```javascript
-keeper.randomBase64(16);
-// mb5cIAM3NEHGd2josTWNnQ==
+keeper
+  .randomBase64(16)
+  .then(result => {
+    console.log(result);
+    // mb5cIAM3NEHGd2josTWNnQ==
+  });
 ```
 
 
@@ -77,8 +117,12 @@ Generates a HMAC-MD5 for the given key and value. Returns the hash in the encodi
 var key = 'foobar';
 var body = 'The quick brown fox';
 
-keeper.hmacMd5(key, body);
-// ccff21844ddf2e55e8ede3a08a0dc8f2
+keeper
+  .hmacMd5(key, body)
+  .then(result => {
+    console.log(result);
+    // ccff21844ddf2e55e8ede3a08a0dc8f2
+  });
 ```
 
 
@@ -88,8 +132,12 @@ Generates a HMAC-SHA1 for the given key and value. Returns the hash in the encod
 var key = 'foobar';
 var body = 'The quick brown fox';
 
-keeper.hmacSha1(key, body);
-// cc05d9cab61c435ddd385cfcb24f1cad24dd3c33
+keeper
+  .hmacSha1(key, body)
+  .then(result => {
+    console.log(result);
+    // cc05d9cab61c435ddd385cfcb24f1cad24dd3c33
+  });
 ```
 
 
@@ -99,8 +147,12 @@ Generates a HMAC-SHA256 for the given key and value. Returns the hash in the enc
 var key = 'foobar';
 var body = 'The quick brown fox';
 
-keeper.hmacSha256(key, body);
-// c8b664809ce67a2a07754c2ca31f726fb11363d86aab26f094656c58239e2501
+keeper
+  .hmacSha256(key, body)
+  .then(result => {
+    console.log(result);
+    // c8b664809ce67a2a07754c2ca31f726fb11363d86aab26f094656c58239e2501
+  });
 ```
 
 
@@ -111,11 +163,19 @@ var salt = 'random_salt';
 var pass = 'strongPass';
 var rounds = 15;
 
-keeper.pbkdf2Hash(pass, salt, rounds);
-// LLYkQP/OJXpmEmEN9WTlzbYcJKofsBa+Qh9MHtpVdoB8OLeTKYcVGSK7QZrJcUFrtph3eWTCvDg=
+keeper
+  .pbkdf2Hash(pass, salt, rounds)
+  .then(result => {
+    console.log(result);
+    // LLYkQP/OJXpmEmEN9WTlzbYcJKofsBa+Qh9MHtpVdoB8OLeTKYcVGSK7QZrJcUFrtph3eWTCvDg=
+  });
 
-keeper.pbkdf2Hash(pass, salt, rounds, 56, 'md5', 'hex');
-// b411a21e7af8bbe603bb358f0d81e53cb4a6f3beb056fc6a6d95fd64df3c29c102048b83761b4b98880469a51b41289c69bb0edcc566bcd1
+keeper
+  .pbkdf2Hash(pass, salt, rounds, 56, 'md5', 'hex')
+  .then(result => {
+    console.log(result);
+    // b411a21e7af8bbe603bb358f0d81e53cb4a6f3beb056fc6a6d95fd64df3c29c102048b83761b4b98880469a51b41289c69bb0edcc566bcd1
+  });
 ```
 
 
@@ -125,8 +185,85 @@ Generates a bcrypt hash with the given value and number of rounds.
 var pass = 'strongPass';
 var rounds = 12;
 
-keeper.bcryptHash(pass, rounds)
-// $2a$12$TMRjxM7AOqgWQBvKwuEAeOpVtBfRz06nU6NeDccb6S/lSML4g/0zG
+keeper
+  .bcryptHash(pass, rounds)
+  .then(result => {
+    console.log(result);
+    // $2a$12$TMRjxM7AOqgWQBvKwuEAeOpVtBfRz06nU6NeDccb6S/lSML4g/0zG
+  });
+```
+
+
+#### #bcryptCompare(given:String, hash:String)
+Compares a given plaintext string with a given bcrypt hash.
+```javascript
+var pass = 'strongPass';
+let passTwo = 'passStrong';
+let hash = '$2a$12$TMRjxM7AOqgWQBvKwuEAeOpVtBfRz06nU6NeDccb6S/lSML4g/0zG';
+
+keeper
+  .bcryptCompare(pass, hash)
+  .then(result => {
+    console.log(result);
+    // true
+  });
+
+keeper
+  .bcryptCompare(passTwo, hash)
+  .then(result => {
+    console.log(result);
+    // false
+  });
+```
+
+
+#### #argonHash(val:String[, options:Object])
+Generates an argon2 hash with the given value and options.
+```javascript
+var pass = 'strongPass';
+let options = {
+  timeCost: 4,
+  memoryCost: 13,
+  parallelism: 2,
+  argon2d: true
+};
+
+keeper
+  .argonHash(pass)
+  .then(result => {
+    console.log(result);
+    // $argon2i$v=19$m=4096,t=3,p=1$ePoXGC034EEZSpoNZtB+zw$EUqaXRHyMjjOyNSy3TdI+f54om5JXUpKDDqYdLD8rlU
+  });
+
+keeper
+  .argonHash(pass, options)
+  .then(result => {
+    console.log(result);
+    // $argon2d$v=19$m=8192,t=4,p=2$qm+sqtF67wyIXGyWPm3gug$P/Sesb2toDUIYOMEqvTDJZ26l3y9Ajx73XUmj/Pt2UY
+  });
+```
+
+
+#### #argonCompare(given:String, hash:String)
+Compares a given plaintext string with a given argon hash.
+```javascript
+var pass = 'strongPass';
+let passTwo = 'passStrong';
+let hash = '$argon2i$v=19$m=4096,t=3,p=1$ePoXGC034EEZSpoNZtB+zw$EUqaXRHyMjjOyNSy3TdI+f54om5JXUpKDDqYdLD8rlU';
+
+keeper
+  .argonCompare(pass, hash)
+  .then(result => {
+    console.log(result);
+    // true
+  });
+
+keeper
+  .argonCompare(passTwo, hash)
+  .then(result => {
+    console.log(result);
+    // false
+  });
 ```
 
 
--- test/test.js
+++ test/test.js
@@ -1,305 +1,309 @@
-var chai   = require('chai');
-var assert = chai.assert;
+'use strict';
 
-var keeper = require('./../');
+const chai   = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const assert = chai.assert;
 
+chai.use(chaiAsPromised);
 
+const keeper = require('./../');
 
-suite('Cryptkeeper', function() {
+suite('Cryptkeeper', () => {
 
-  suite('#base64Encode', function() {
+  suite('#base64Encode', () => {
 
-    test('throws for unknown value', function() {
-      assert.throws(function() {
-        keeper.base64Encode();
-      });
+    test('throws for unknown value', () => {
+      assert.isRejected(keeper.base64Encode());
     });
 
-    test('returns string for given string', function() {
-      var actual = keeper.base64Encode('foo:bar');
-      assert.isString(actual);
+    test('returns string for given string', () => {
+      assert.eventually.isString(keeper.base64Encode('foo:bar'));
     });
 
-    test('returns string for given object', function() {
-      var actual = keeper.base64Encode({foo: 'bar'});
-      assert.isString(actual);
+    test('returns string for given object', () => {
+      assert.eventually.isString(keeper.base64Encode({foo: 'bar'}));
     });
 
-    test('returns string for given number', function() {
-      var actual = keeper.base64Encode(156);
-      assert.isString(actual);
+    test('returns string for given number', () => {
+      assert.eventually.isString(keeper.base64Encode(156));
     });
 
-    test('returns string for given Buffer', function() {
-      var actual = keeper.base64Encode(new Buffer('foo:bar'));
-      assert.isString(actual);
+    test('returns string for given Buffer', () => {
+      assert.eventually.isString(keeper.base64Encode(new Buffer('foo:bar')));
     });
 
-    test('returns correct value', function() {
-      var actual = keeper.base64Encode('Hello world');
-      var expected = 'SGVsbG8gd29ybGQ=';
-      assert.strictEqual(actual, expected);
+    test('returns correct value', () => {
+      let expected = 'SGVsbG8gd29ybGQ=';
+      assert.eventually.strictEqual(keeper.base64Encode('Hello world'), expected);
     });
   });
 
-  suite('#base64Decode', function() {
+  suite('#base64Decode', () => {
 
-    test('throws for unknown value', function() {
-      assert.throws(function() {
-        keeper.base64Decode();
-      });
+    test('throws for unknown value', () => {
+      assert.isRejected(keeper.base64Decode());
     });
 
-    test('throws for non-string value', function() {
-      assert.throws(function() {
-        keeper.base64Decode({foo: 'bar'});
-      });
+    test('throws for non-string value', () => {
+      assert.isRejected(keeper.base64Decode({foo: 'bar'}));
     });
 
-    test('returns string for given string', function() {
-      var actual = keeper.base64Decode('SGVsbG8gd29ybGQ=');
-      assert.isString(actual);
+    test('returns string for given string', () => {
+      assert.eventually.isString(keeper.base64Decode('SGVsbG8gd29ybGQ='));
     });
 
-    test('returns correct value', function() {
-      var actual = keeper.base64Decode('SGVsbG8gd29ybGQ=');
-      var expected = 'Hello world';
-      assert.strictEqual(actual, expected);
+    test('returns correct value', () => {
+      let expected = 'Hello world';
+      assert.eventually.strictEqual(keeper.base64Decode('SGVsbG8gd29ybGQ='), expected);
     });
   });
 
-  suite('#generateV4UUID', function() {
+  suite('#generateV4UUID', () => {
 
-    test('returns string', function() {
-      var actual = keeper.generateV4UUID();
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.generateV4UUID());
     });
 
-    test('returns correct pattern', function() {
-      var re = /[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/;
-      var actual = keeper.generateV4UUID();
-      assert.match(actual, re);
+    test('returns correct pattern', () => {
+      let re = /[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/;
+      assert.eventually.match(keeper.generateV4UUID(), re);
     });
   });
 
-  suite('#randomNumber', function() {
+  suite('#randomNumber', () => {
 
-    test('returns number', function() {
-      var actual = keeper.randomNumber();
-      assert.isNumber(actual);
+    test('returns number', () => {
+      assert.eventually.isNumber(keeper.randomNumber());
     });
 
-    test('in no arg range', function() {
-      var actual = keeper.randomNumber();
-      assert.isTrue(actual >= 0);
-
-      actual = keeper.randomNumber();
-      assert.isTrue(actual <= 1);
+    test('in no arg range', () => {
+      assert.eventually.isAtLeast(keeper.randomNumber(), 0);
+      assert.eventually.isAtMost(keeper.randomNumber(), 1);
     });
 
-    test('in one arg range', function() {
-      var actual = keeper.randomNumber(10);
-      assert.isTrue(actual >= 0);
-
-      actual = keeper.randomNumber(10);
-      assert.isTrue(actual <= 10);
+    test('in one arg range', () => {
+      assert.eventually.isAtLeast(keeper.randomNumber(10), 0);
+      assert.eventually.isAtMost(keeper.randomNumber(10), 10);
     });
 
-    test('in two arg range', function() {
-      var actual = keeper.randomNumber(10, 20);
-      assert.isTrue(actual >= 10);
-
-      actual = keeper.randomNumber(10, 20);
-      assert.isTrue(actual <= 20);
+    test('in two arg range', () => {
+      assert.eventually.isAtLeast(keeper.randomNumber(10, 20), 10);
+      assert.eventually.isAtMost(keeper.randomNumber(10, 20), 20);
     });
   });
 
-  suite('#randomHex', function() {
+  suite('#randomHex', () => {
 
-    test('throws for unknown number of bytes', function() {
-      assert.throws(function() {
-        keeper.randomHex();
-      });
+    test('throws for unknown number of bytes', () => {
+      assert.isRejected(keeper.randomHex());
     });
 
-    test('returns string', function() {
-      var actual = keeper.randomHex(16);
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.randomHex(16));
     });
 
-    test('returns correct pattern', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.randomHex(16);
-      assert.match(actual, re);
+    test('returns correct pattern', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.match(keeper.randomHex(16), re);
     });
   });
 
-  suite('#randomBase64', function() {
+  suite('#randomBase64', () => {
 
-    test('throws for unknown number of bytes', function() {
-      assert.throws(function() {
-        keeper.randomBase64();
-      });
+    test('throws for unknown number of bytes', () => {
+      assert.isRejected(keeper.randomBase64());
     });
 
-    test('returns string', function() {
-      var actual = keeper.randomBase64(16);
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.randomBase64(16));
     });
   });
 
-  suite('#hmacMd5', function() {
+  suite('#hmacMd5', () => {
 
-    test('throws for unknown key', function() {
-      assert.throws(function() {
-        keeper.hmacMd5(null, 'someval');
-      });
+    test('throws for unknown key', () => {
+      assert.isRejected(keeper.hmacMd5(null, 'someval'));
     });
 
-    test('throws for unknown val', function() {
-      assert.throws(function() {
-        keeper.hmacMd5('somekey', null);
-      });
+    test('throws for unknown val', () => {
+      assert.isRejected(keeper.hmacMd5('somekey', null));
     });
 
-    test('returns string', function() {
-      var actual = keeper.hmacMd5('somekey', 'someval');
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.hmacMd5('somekey', 'someval'));
     });
 
-    test('returns hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.hmacMd5('somekey', 'someval');
-      assert.match(actual, re);
+    test('returns hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.match(keeper.hmacMd5('somekey', 'someval'), re);
     });
 
-    test('returns not hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.hmacMd5('somekey', 'someval', 'base64');
-      assert.notMatch(actual, re);
+    test('returns not hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.notMatch(keeper.hmacMd5('somekey', 'someval', 'base64'), re);
     });
   });
 
-  suite('#hmacSha1', function() {
+  suite('#hmacSha1', () => {
 
-    test('throws for unknown key', function() {
-      assert.throws(function() {
-        keeper.hmacSha1(null, 'someval');
-      });
+    test('throws for unknown key', () => {
+      assert.isRejected(keeper.hmacSha1(null, 'someval'));
     });
 
-    test('throws for unknown val', function() {
-      assert.throws(function() {
-        keeper.hmacSha1('somekey', null);
-      });
+    test('throws for unknown val', () => {
+      assert.isRejected(keeper.hmacSha1('somekey', null));
     });
 
-    test('returns string', function() {
-      var actual = keeper.hmacSha1('somekey', 'someval');
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.hmacSha1('somekey', 'someval'));
     });
 
-    test('returns hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.hmacSha1('somekey', 'someval');
-      assert.match(actual, re);
+    test('returns hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.match(keeper.hmacSha1('somekey', 'someval'), re);
     });
 
-    test('returns not hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.hmacSha1('somekey', 'someval', 'base64');
-      assert.notMatch(actual, re);
+    test('returns not hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.notMatch(keeper.hmacSha1('somekey', 'someval', 'base64'), re);
     });
   });
 
-  suite('#hmacSha256', function() {
+  suite('#hmacSha256', () => {
 
-    test('throws for unknown key', function() {
-      assert.throws(function() {
-        keeper.hmacSha256(null, 'someval');
-      });
+    test('throws for unknown key', () => {
+      assert.isRejected(keeper.hmacSha256(null, 'someval'));
     });
 
-    test('throws for unknown val', function() {
-      assert.throws(function() {
-        keeper.hmacSha256('somekey', null);
-      });
+    test('throws for unknown val', () => {
+      assert.isRejected(keeper.hmacSha256('somekey', null));
     });
 
-    test('returns string', function() {
-      var actual = keeper.hmacSha256('somekey', 'someval');
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.hmacSha256('somekey', 'someval'));
     });
 
-    test('returns hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.hmacSha256('somekey', 'someval');
-      assert.match(actual, re);
+    test('returns hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.match(keeper.hmacSha256('somekey', 'someval'), re);
     });
 
-    test('returns not hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.hmacSha256('somekey', 'someval', 'base64');
-      assert.notMatch(actual, re);
+    test('returns not hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.notMatch(keeper.hmacSha256('somekey', 'someval', 'base64'), re);
     });
   });
 
-  suite('#pbkdf2Hash', function() {
+  suite('#pbkdf2Hash', () => {
 
-    test('throws for unknown value', function() {
-      assert.throws(function() {
-        keeper.pbkdf2Hash(null, 'somesalt', 10);
-      });
+    test('throws for unknown value', () => {
+      assert.isRejected(keeper.pbkdf2Hash(null, 'somesalt', 10));
     });
 
-    test('throws for unknown salt', function() {
-      assert.throws(function() {
-        keeper.pbkdf2Hash('someval', null, 10);
-      });
+    test('throws for unknown salt', () => {
+      assert.isRejected(keeper.pbkdf2Hash('someval', null, 10));
     });
 
-    test('throws for unknown rounds', function() {
-      assert.throws(function() {
-        keeper.pbkdf2Hash('someval', 'somesalt');
-      });
+    test('throws for unknown rounds', () => {
+      assert.isRejected(keeper.pbkdf2Hash('someval', 'somesalt'));
     });
 
-    test('returns string', function() {
-      var actual = keeper.pbkdf2Hash('someval', 'somesalt', 10);
-      assert.isString(actual);
+    test('returns string', () => {
+      assert.eventually.isString(keeper.pbkdf2Hash('someval', 'somesalt', 10));
     });
 
-    test('returns not hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.pbkdf2Hash('someval', 'somesalt', 10);
-      assert.notMatch(actual, re);
+    test('returns not hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.notMatch(keeper.pbkdf2Hash('someval', 'somesalt', 10), re);
     });
 
-    test('returns hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.pbkdf2Hash('someval', 'somesalt', 10, 56, 'sha512', 'hex');
-      assert.match(actual, re);
+    test('returns hex', () => {
+      let re = /^[a-f0-9]+$/;
+      assert.eventually.match(keeper.pbkdf2Hash('someval', 'somesalt', 10, 56, 'sha512', 'hex'), re);
     });
   });
 
-  suite('#bcryptHash', function() {
+  suite('#bcryptHash', () => {
 
-    test('throws for unknown value', function() {
-      assert.throws(function() {
-        keeper.bcryptHash(null, 5);
-      });
+    test('throws for unknown value', () => {
+      assert.isRejected(keeper.bcryptHash(null, 5));
     });
 
-    test('throws for unknown rounds', function() {
-      assert.throws(function() {
-        keeper.bcryptHash('someval');
-      });
+    test('throws for unknown rounds', () => {
+      assert.isRejected(keeper.bcryptHash('someval'));
     });
 
-    test('returns not hex', function() {
-      var re = /^[a-f0-9]+$/;
-      var actual = keeper.bcryptHash('someval', 5);
-      assert.notMatch(actual, re);
+    test('returns correct hash', () => {
+      let expected = '$2a$10$fWyqxuj6NWll9sokndTVoeakdZhyhBDkucLHhKDdEL0wqL1YkiGou';
+      assert.eventually.strictEqual(keeper.bcryptHash('unit&test', 10), expected);
+    });
+  });
+
+  suite('#bcryptCompare', () => {
+
+    test('throws for unknown given', () => {
+      assert.isRejected(keeper.bcryptCompare(null, ''));
+    });
+
+    test('throws for unknown hash', () => {
+      assert.isRejected(keeper.bcryptCompare('someval'));
+    });
+
+    test('returns true for match', () => {
+      let hash = '$2a$10$fWyqxuj6NWll9sokndTVoeakdZhyhBDkucLHhKDdEL0wqL1YkiGou';
+      let given = 'unit&test';
+      assert.eventually.isTrue(keeper.bcryptCompare(given, hash));
+    });
+
+    test('returns false for mismatch', () => {
+      let hash = '$2a$10$fWyqxuj6NWll9sokndTVoeakdZhyhBDkucLHhKDdEL0wqL1YkiGou';
+      let given = 'test&unit';
+      assert.eventually.isTrue(keeper.bcryptCompare(given, hash));
+    });
+  });
+
+  suite('#argonHash', () => {
+
+    test('throws for unknown value', () => {
+      assert.isRejected(keeper.argonHash());
+    });
+
+    test('returns correct hash for default options', () => {
+      let expected = '$argon2i$v=19$m=4096,t=3,p=1$/u30wNNYOfYADLFWn+PMnQ$6vbaaXlHb2uC2jxTX7RsAJivyEIfnT0sC2w3kbzupQQ';
+      assert.eventually.strictEqual(keeper.argonHash('unit&test'), expected);
+    });
+
+    test('returns correct hash for given options', () => {
+      let expected = '$argon2d$v=19$m=8192,t=4,p=2$LaxrvchRgoqf0WAiaxB5ZQ$0u9pkixo5S1SznL/k8hxc95xa1InoopKhCiPJ1izlTw';
+      let options = { timeCost: 4, memoryCost: 13, parallelism: 2, argon2d: true };
+      assert.eventually.strictEqual(keeper.argonHash('unit&test'), expected);
+    });
+  });
+
+  suite('#argonCompare', () => {
+
+    test('throws for unknown given', () => {
+      assert.isRejected(keeper.argonCompare(null, ''));
+    });
+
+    test('throws for unknown hash', () => {
+      assert.isRejected(keeper.argonCompare('someval'));
+    });
+
+    test('returns true for match', () => {
+      let hash = '$argon2i$v=19$m=4096,t=3,p=1$/u30wNNYOfYADLFWn+PMnQ$6vbaaXlHb2uC2jxTX7RsAJivyEIfnT0sC2w3kbzupQQ';
+      let hash2 = '$argon2d$v=19$m=8192,t=4,p=2$LaxrvchRgoqf0WAiaxB5ZQ$0u9pkixo5S1SznL/k8hxc95xa1InoopKhCiPJ1izlTw';
+      let given = 'unit&test';
+      assert.eventually.isTrue(keeper.argonCompare(given, hash));
+      assert.eventually.isTrue(keeper.argonCompare(given, hash2));
+    });
+
+    test('returns false for mismatch', () => {
+      let hash = '$argon2i$v=19$m=4096,t=3,p=1$/u30wNNYOfYADLFWn+PMnQ$6vbaaXlHb2uC2jxTX7RsAJivyEIfnT0sC2w3kbzupQQ';
+      let hash2 = '$argon2d$v=19$m=8192,t=4,p=2$LaxrvchRgoqf0WAiaxB5ZQ$0u9pkixo5S1SznL/k8hxc95xa1InoopKhCiPJ1izlTw';
+      let given = 'test&unit';
+      assert.eventually.isTrue(keeper.argonCompare(given, hash));
+      assert.eventually.isTrue(keeper.argonCompare(given, hash2));
     });
   });
 });
